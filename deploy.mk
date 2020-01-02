9faedc8bcc99db7e7f4678652a7c6e36232da72d
Caio Kinzel Filho <caiokf@gmail.com>
2020-01-02 18:18:39 +0300

    move if ENV not set into make targets

--- deploy.mk
+++ deploy.mk
@@ -1,23 +1,20 @@
 include github.com/InDebted/make/node
 include github.com/InDebted/make/build
 
-.PHONY: deploy
-
-ifndef ENV
-deploy:; $(error ENV environment variable is required)
-deploy/%:; $(error ENV environment variable is required)
-else
-
 serverless-deploy := npx --no-install sls deploy --aws-s3-accelerate --conceal --verbose --stage $(ENV)
 
 # Deploys all handlers to ENV
 deploy: $(all_handlers) | node_dependencies
+	$(if $(ENV),,$(error ENV environment variable is required))
+
 	$(call _info,Deploying $(notdir $(CURDIR)) to $(ENV) environment…)
 	@$(serverless-deploy)
+.PHONY: deploy
 
 # Deploy specified handler to ENV
 deploy/%: $(dist_dir)/% | node_dependencies
+	$(if $(ENV),,$(error ENV environment variable is required))
+
 	$(call _info,Deploying $* to $(ENV) environment…)
 	@$(serverless-deploy) --function $*
-
-endif
+.PHONY: deploy/%
