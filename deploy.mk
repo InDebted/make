36ab70f22ec6369e87a2dae52ffa28499287e6c5
Caio Kinzel Filho <caiokf@gmail.com>
2020-01-02 17:41:27 +0300

    comments on deploy

--- deploy.mk
+++ deploy.mk
@@ -10,10 +10,12 @@ else
 
 serverless-deploy := npx --no-install sls deploy --aws-s3-accelerate --conceal --verbose --stage $(ENV)
 
+# Deploys all handlers to ENV
 deploy: $(all_handlers) | node_dependencies
 	$(call _info,Deploying $(notdir $(CURDIR)) to $(ENV) environment…)
 	@$(serverless-deploy)
 
+# Deploy specified handler to ENV
 deploy/%: $(dist_dir)/% | node_dependencies
 	$(call _info,Deploying $* to $(ENV) environment…)
 	@$(serverless-deploy) --function $*
