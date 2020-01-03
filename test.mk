3b85e8c633a04d7c2d623afc48269ba6231aa7fa
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2020-01-03 17:14:12 +1100

    Show go outputs

--- test.mk
+++ test.mk
@@ -1,5 +1,5 @@
 # Runs all tests for all modules
 test:
 	$(call _info,Testing…)
-	@go test -count=1 $$(go list $$(ls -d */ | grep -v 'node_modules/' | awk '{print "./"$$0"..."}') 2>&1 | grep -v '^go: ')
+	@go test -count=1 $$(go list $$(ls -d */ | grep -v 'node_modules/' | awk '{print "./"$$0"..."}'))
 .PHONY: test
