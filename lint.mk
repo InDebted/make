86d21b84b3eef3a6d2dabbc64b677788ac028c52
Caio Kinzel Filho <caiokf@gmail.com>
2020-01-06 07:01:07 +0300

    lint targets message on finish

--- lint.mk
+++ lint.mk
@@ -7,19 +7,23 @@ lint: lint-dependencies lint-format lint-imports lint-source
 lint-dependencies:
 	$(call _info,Linting dependencies…)
 	@go mod verify
+	$(call _info,Linting dependencies finished!)
 .PHONY: lint-dependencies
 
 lint-format:
 	$(call _info,Linting format…)
 	@gofmt -l $(go_files) | tee /dev/fd/2 | xargs test -z
+	$(call _info,Linting format finished!)
 .PHONY: lint-format
 
 lint-imports:
 	$(call _info,Linting imports…)
 	@goimports -l $(go_files) | tee /dev/fd/2 | xargs test -z
+	$(call _info,Linting imports finished!)
 .PHONY: lint-imports
 
 lint-source:
 	$(call _info,Linting source…)
 	@for file in $(go_files); do golint -set_exit_status "$$file" 2>&1; done
+	$(call _info,Linting source finished!)
 .PHONY: lint-source
