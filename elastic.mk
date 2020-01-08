ceaccf9ddead6451cac16bd20fe905b2e63e7034
Caio Kinzel Filho <caiokf@gmail.com>
2020-01-08 13:19:57 +0300

    elastic search related targets

---
+++ elastic.mk
@@ -0,0 +1,7 @@
+export ELASTICSEARCH_URL ?= http://elasticsearch:9200
+
+# Wait for Elastic Search to be up
+elastic.is-up:
+	$(call _info,Waiting for Elastic Search…)
+	@dockerize -wait ${ELASTICSEARCH_URL}/_cat/health -timeout 60s
+.PHONY: elastic.is-up
