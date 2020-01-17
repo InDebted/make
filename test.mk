6a1874e6fda3681bcb9ab96c214b29d121352950
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2020-01-17 14:26:08 +1100

    Cater for repositories with go files in the root directory
    Also ditch `go list | grep -v` in favor of `git ls-files`.

--- test.mk
+++ test.mk
@@ -1,5 +1,8 @@
 # Runs all tests for all modules
 test:
 	$(call _info,Testing…)
-	@go test -count=1 $$(go list $$(ls -d */ | grep -v 'node_modules/' | awk '{print "./"$$0"..."}'))
+	@go test -count=1 $$({\
+		(test -z "$$(find . -maxdepth 1 -type f -name '*_test.go')" || echo .) & \
+		{ git ls-files '**/*_test.go' & git ls-files -o --exclude-standard '**/*_test.go'; } | cut -d/ -f1 | uniq | awk '{print "./"$$0"/..."}' \
+	;})
 .PHONY: test
