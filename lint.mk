3d2a45560c37a3647666ca3a5c8e145cc55c84fa
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2020-01-20 00:44:16 +1100

    Supress info messages

--- lint.mk
+++ lint.mk
@@ -5,23 +5,23 @@ lint: lint.dependencies lint.format lint.imports lint.source
 lint.dependencies:
 	@$(call _info,Linting dependencies…)
 	@go mod verify
-	$(call _info,Linting dependencies finished!)
+	@$(call _info,Linting dependencies finished!)
 .PHONY: lint.dependencies
 
 lint.format:
 	@$(call _info,Linting format…)
 	@gofmt -l $(shell $(call git-ls,'*.go')) | tee /dev/fd/2 | xargs test -z
-	$(call _info,Linting format finished!)
+	@$(call _info,Linting format finished!)
 .PHONY: lint.format
 
 lint.imports:
 	@$(call _info,Linting imports…)
 	@goimports -l $(shell $(call git-ls,'*.go')) | tee /dev/fd/2 | xargs test -z
-	$(call _info,Linting imports finished!)
+	@$(call _info,Linting imports finished!)
 .PHONY: lint.imports
 
 lint.source:
 	@$(call _info,Linting source…)
 	@$(call git-ls,'*.go') | xargs -n1 golint -set_exit_status 2>&1
-	$(call _info,Linting source finished!)
+	@$(call _info,Linting source finished!)
 .PHONY: lint.source
