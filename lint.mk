4601bbec08a00ed194d61a2a6022d735bcd13c8f
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2020-01-19 23:54:59 +1100

    Standartize namespaced phony targets with `.`

--- lint.mk
+++ lint.mk
@@ -1,27 +1,27 @@
 # Lints the source files looking for anti-patterns
-lint: lint-dependencies lint-format lint-imports lint-source
+lint: lint.dependencies lint.format lint.imports lint.source
 .PHONY: lint
 
-lint-dependencies:
+lint.dependencies:
 	@$(call _info,Linting dependencies…)
 	@go mod verify
 	$(call _info,Linting dependencies finished!)
-.PHONY: lint-dependencies
+.PHONY: lint.dependencies
 
-lint-format:
+lint.format:
 	@$(call _info,Linting format…)
 	@gofmt -l $(shell $(call git-ls,'*.go')) | tee /dev/fd/2 | xargs test -z
 	$(call _info,Linting format finished!)
-.PHONY: lint-format
+.PHONY: lint.format
 
-lint-imports:
+lint.imports:
 	@$(call _info,Linting imports…)
 	@goimports -l $(shell $(call git-ls,'*.go')) | tee /dev/fd/2 | xargs test -z
 	$(call _info,Linting imports finished!)
-.PHONY: lint-imports
+.PHONY: lint.imports
 
-lint-source:
+lint.source:
 	@$(call _info,Linting source…)
 	@$(call git-ls,'*.go') | xargs -n1 golint -set_exit_status 2>&1
 	$(call _info,Linting source finished!)
-.PHONY: lint-source
+.PHONY: lint.source
