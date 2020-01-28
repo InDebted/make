41820a7d24bd81171291918e919826a728b4bc2b
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2020-01-28 23:05:05 +1100

    Group output into one `test -z` for test files

--- test.mk
+++ test.mk
@@ -2,7 +2,7 @@
 test:
 	@$(call _info,Testing…)
 	@go test -count=1 $$({\
-		$(call git-ls,':(glob)*_test.go') | xargs test -z || echo . & \
+		$(call git-ls,':(glob)*_test.go') | xargs -I{} test -z "{}" || echo . & \
 		$(call git-ls,'**/*_test.go') | cut -d/ -f1 | uniq | awk '{print "./"$$0"/..."}' \
 	;})
 .PHONY: test
